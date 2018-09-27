cf4c8ba1e5fd08267e8c792dbba5cfea12549962
casper <ycasperinho@gmail.com>
2018-09-27 22:43:54 +0300

    make links case-insensitively unique

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
-    url { Faker::Internet.url }
-    shortened { Faker::FamilyGuy.character }
+    url { Faker::Internet.unique.url }
+    shortened { Faker::Name.unique.name }
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
   it { should validate_presence_of(:url) }
+  it { should validate_uniqueness_of(:url).case_insensitive }
 
   it { should validate_presence_of(:shortened) }
+  it { should validate_uniqueness_of(:shortened).case_insensitive }
 end
