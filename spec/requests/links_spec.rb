de3cde04cf404b436863f6d7a414173b9ed3ea4c
casper <ycasperinho@gmail.com>
2018-09-27 19:44:33 +0300

    tests to match new routes

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Links API', type: :request do
   let!(:links) { create_list(:link, 10) }
   let(:link_id) { link.first.id }
 
-  describe 'GET /api/links' do
-    before { get '/api/links' }
+  describe 'all' do
+    before { get links_index_path }
 
     it 'returns all the links' do
       expect(json).not_to be_empty
@@ -14,6 +14,39 @@ RSpec.describe 'Links API', type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'store link' do
+    # valid payload
+    let(:valid_attributes) { {
+      url: Faker::Internet.url,
+      shortened: Faker::FamilyGuy.character
+    } }
+
+    context 'when the request is valid' do
+      before { post links_store_path, params: valid_attributes }
+
+      it 'stores a link' do
+        expect(json['url']).to eq(valid_attributes[:url])
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post links_store_path, params: { url: valid_attributes[:url] } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Shortened can't be blank/)
+      end
     end
   end
 end
