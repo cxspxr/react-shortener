3a679f6070285eebfc03ece8ccc4bca57f8f6a57
casper <ycasperinho@gmail.com>
2018-09-28 11:49:02 +0300

    send url with base, fix test

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -31,18 +31,19 @@ RSpec.describe 'Links API', type: :request do
       shortened: Faker::Number.number
     } }
 
-    before { post links_get_path, params: { url: valid_attributes[:url] }}
-
     context 'when there is a link' do
       before { post links_store_path, params: valid_attributes }
+      before { post links_get_path, params: { url: valid_attributes[:url] }}
 
       it 'returns shortened for it' do
         expect(json).not_to be_empty
-        expect(json['shortened']).to eq(valid_attributes[:shortened])
+        expect(json['shortened']).to eq("#{request.base_url}/#{valid_attributes[:shortened]}")
       end
     end
 
     context 'when there is no link' do
+      before { post links_get_path, params: { url: valid_attributes[:url] }}
+
       it 'returns nothing' do
         expect(response.body).to be_empty
       end
