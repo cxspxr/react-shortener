27b1a463dfbf7b95a5db341772054f3bb9fd2f62
casper <ycasperinho@gmail.com>
2018-09-28 04:22:08 +0300

    fake shortened as a single word

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
     url { Faker::Internet.unique.url }
-    shortened { Faker::Name.unique.name }
+    shortened { Faker::Number.unique.number }
   end
 end
